89586892e68fd49836d08deece687dafac7da6f9
P26pveilleux <144148685+P26pveilleux@users.noreply.github.com>
2024-02-05 09:02:06 -0500

    new changes

--- Lab11-1.py
+++ Lab11-1.py
@@ -1,10 +1,12 @@
 # Lab 1 
 def even_number_in_range(num_A,num_B):
     return [num for num in range(num_A,num_B + 1) if num % 2 == 0]
+# trying to set the range 
 name = 0 
 if name =="main":
     start_range = int(input("start number"))
     end_range = int(input("end number"))
+#starting and ending number for the code 
 print("evan numbers")
 clean_string = 0 
 #Lab 2 
